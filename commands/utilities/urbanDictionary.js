ff2bfd8f43b81b7e6185b5c7b2ab89e6b12e6de4
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-02 13:19:53 +0900

    Adding new Commands

--- commands/utilities/urbanDictionary.js
+++ commands/utilities/urbanDictionary.js
@@ -1,36 +1,42 @@
-const { Client, Message, MessageEmbed } = require('discord.js');
-const axios = require('axios');
+const { Client, Message, MessageEmbed } = require("discord.js");
+const axios = require("axios");
 
 module.exports = {
-    name: 'urban',
-    /** 
-     * @param {Client} client 
-     * @param {Message} message 
-     * @param {String[]} args 
-     */
-    run: async(client, message, args) => {
-        let query = args.join(" ");
-        if(!query) return message.reply('Please specify a word to search for!');
+  name: "urban",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    let query = args.join(" ");
+    if (!query) return message.reply("Please specify a word to search for!");
 
-        query = encodeURIComponent(query);
+    query = encodeURIComponent(query);
 
-        const { data: { list }} = await axios.get(`https://api.urbandictionary.com/v0/define?term=${query}`);
+    const {
+      data: { list },
+    } = await axios.get(
+      `https://api.urbandictionary.com/v0/define?term=${query}`
+    );
 
-        const [ answer ] = list;
+    const [answer] = list;
 
-        message.channel.send(
-            new MessageEmbed()
-                .setTitle(answer.word)
-                .setURL(answer.permalink)
-                .setColor('RANDOM')
-                .addField("DEFINITION", trim(answer.definition))
-                .addField("EXAMPLE", trim(answer.example))
-                .addField("RATINGS", `${answer.thumbs_up} 👍 || ${answer.thumbs_down} 👎`)
+    message.channel.send(
+      new MessageEmbed()
+        .setTitle(answer.word)
+        .setURL(answer.permalink)
+        .setColor("RANDOM")
+        .addField("DEFINITION", trim(answer.definition))
+        .addField("EXAMPLE", trim(answer.example))
+        .addField(
+          "RATINGS",
+          `${answer.thumbs_up} 👍 || ${answer.thumbs_down} 👎`
         )
-
-    }
-}
+    );
+  },
+};
 
 function trim(input) {
-    return input.length > 1024 ? `${input.slice(0, 1020)} ...` : input
+  return input.length > 1024 ? `${input.slice(0, 1020)} ...` : input;
 }
