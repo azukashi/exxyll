16d8473dda5de3a83c5f010ed95c8fc2b5e87df6
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-02 17:20:36 +0900

    Fix Fight with Button Command

--- commands/Games/fightWithButton.js
+++ commands/Games/fightWithButton.js
@@ -1,19 +1,31 @@
-const oppenent = message.mentions.users.first();
-if (!oppenent)
-  return message.channel.send(`Please mention who you want to fight`);
-const { fight } = require("weky");
-const x = new fight({
-  client: client,
-  message: message,
-  acceptMessage: "Click to fight with <@" + message.author + ">",
-  challenger: message.author,
-  opponent: message.mentions.users.first(),
-  hitButtonText: "HIT",
-  hitButtonColor: "red",
-  healButtonText: "HEAL",
-  healButtonColor: "green",
-  cancelButtonText: "CANCEL",
-  cancelButtonColor: "blurple",
-});
-x.start();
-//ERRORS? Join our support server https://discord.gg/2EZSpxNB5z (Weky Npm Official)
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { MessageButton } = require("discord-buttons");
+
+module.exports = {
+  name: "fight",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const oppenent = message.mentions.users.first();
+    if (!oppenent)
+      return message.channel.send(`Please mention who you want to fight`);
+    const { fight } = require("weky");
+    const x = new fight({
+      client: client,
+      message: message,
+      acceptMessage: "Click to fight with <@" + message.author + ">",
+      challenger: message.author,
+      opponent: message.mentions.users.first(),
+      hitButtonText: "HIT",
+      hitButtonColor: "red",
+      healButtonText: "HEAL",
+      healButtonColor: "green",
+      cancelButtonText: "CANCEL",
+      cancelButtonColor: "blurple",
+    });
+    x.start();
+  },
+};
