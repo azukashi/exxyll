d9a1b1e768d3f25da01e47def068ed86a096b594
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-12 16:20:16 +0900

    Try console.log inside play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -30,7 +30,7 @@ module.exports = {
             `Song Name : ${song.name}nDuration : ${song.formattedDuration}\nAdded by : ${song.user}`
         )
       )
-      .setThumbnail(queue.songs.map((song) => song.thumbnail[1]))
+      .setThumbnail(queue.songs.map((song) => song.thumbnail.url))
       .setColor("RED")
       .setFooter(
         message.author.tag,
@@ -38,6 +38,6 @@ module.exports = {
       );
 
     message.channel.send(addedSong).catch((err) => message.channel.send(err));
-    console.log(queue.songs.map((song) => song.thumbnail[1]));
+    console.log(queue.songs.map((song) => song.thumbnail.url));
   },
 };
