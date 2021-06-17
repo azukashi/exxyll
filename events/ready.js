aadbd93925ca919cb30b5a53d2c7d2582bbf78f4
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-18 02:58:58 +0700

    rollback: rollback to streaming rpc

--- events/ready.js
+++ events/ready.js
@@ -2,28 +2,11 @@ const RPC = require("discord-rpc");
 const rpc = new RPC.Client({ transport: "ipc" });
 const client = require("../index");
 const prefix = require("../config.json").prefix;
-const clientId = "818884394416603161";
-const scopes = ["rpc", "rpc.api", "messages.read"];
 
-rpc.on("ready", () => {
-  rpc.setActivity({
-    details: "Exxyll Discord Bot || Type .help",
-    state: "A Multipurpose Discord Bot",
-    startTimestamp: new Date(),
-    largeImageKey: "exxyll",
-    largeImageText: "Exxyll",
-    smallImageKey: "exxyll_v1",
-    smallImageText: "Release v1.0.1",
-    buttons: [
-      {
-        label: "Invite Me",
-        url: "https://discord.com/oauth2/authorize?client_id=848232775798226996&permissions=3222646&scope=bot",
-      },
-      { label: "Visit Website", url: "https://exxyll.github.io" },
-    ],
+client.on("ready", () => {
+  client.user.setActivity(`${prefix}help`, {
+    type: "STREAMING",
+    url: "https://www.twitch.tv/falcxxr",
   });
-
-  console.log("RPC Online!");
+  console.log(`${client.user.username} has logged on!`);
 });
-
-rpc.login({ clientId, scopes });
