bb3f13215b835e101650bc7788ef95e471c4ee31
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-18 02:56:08 +0700

    test: adding scopes

--- events/ready.js
+++ events/ready.js
@@ -2,15 +2,17 @@ const RPC = require("discord-rpc");
 const rpc = new RPC.Client({ transport: "ipc" });
 const client = require("../index");
 const prefix = require("../config.json").prefix;
+const clientId = "818884394416603161";
+const scopes = ["rpc", "rpc.api", "messages.read"];
 
 rpc.on("ready", () => {
   rpc.setActivity({
     details: "Exxyll Discord Bot || Type .help",
     state: "A Multipurpose Discord Bot",
     startTimestamp: new Date(),
-    largeImageKey: "finder",
+    largeImageKey: "exxyll",
     largeImageText: "Exxyll",
-    smallImageKey: "capitan",
+    smallImageKey: "exxyll_v1",
     smallImageText: "Release v1.0.1",
     buttons: [
       {
@@ -24,6 +26,4 @@ rpc.on("ready", () => {
   console.log("RPC Online!");
 });
 
-rpc.login({
-  clientId: "818884394416603161",
-});
+rpc.login({ clientId, scopes });
