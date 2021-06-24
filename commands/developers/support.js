853762019b5782fa98a1439c590bf6e2cd23faf3
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-24 19:56:04 +0700

    chore: nerf id platforms in support.js

--- commands/developers/support.js
+++ commands/developers/support.js
@@ -13,8 +13,9 @@ module.exports = {
   run: async (client, message, args) => {
     const embed = new MessageEmbed()
       .setTitle("Support the Developers!")
-      .setDescription("You can support me with many ways!")
-      .addField(`[Buy me a Coffee](https://ko-fi.com/gifaldyazkaa)`)
+      .setDescription(
+        "Support or Dotate to Support the Developers and Get Premium Perks!\n\n[Buy me a Coffee](https://ko-fi.com/gifaldyazkaa)"
+      )
       .setFooter(message.author.tag)
       .setColor("BLUE")
       .setTimestamp();
