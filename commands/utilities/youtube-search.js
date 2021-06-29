f25c896b20992107f84195513ccdc03bc85af9a1
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-29 08:41:15 +0700

    Adding yt search cmds

---
+++ commands/utilities/youtube-search.js
@@ -0,0 +1,42 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const ytsr = require("ytsr");
+
+module.exports = {
+  name: "youtube-search",
+  aliases: ["yts", "ys"],
+  usage: "<query or smthn>",
+  description: "Search YouTube Video in Discord!",
+  hidden: false,
+  premium: false,
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const query = args.join(" ");
+    if (!query)
+      return message.lineReply("Provide a search for me to search YouTube!");
+
+    const res = await ytsr(query).catch((e) =>
+      message.lineReplyNoMention(`No results found for ${query}`)
+    );
+    const video = res.items.filter((i) => i.type === "video")[0];
+    const embed = new MessageEmbed()
+      .setTitle(video.title)
+      .setURL(video.url)
+      .setImage(video.bestThumbnail.url)
+      .setDescription(video.description ? video.description : "No Description")
+      .addField(
+        `Song Information`,
+        `**Creator**: [${video.author.name}](${video.author.url}) ${
+          video.author.verified ? ":white_check_mark: (Verified)" : "\u200b"
+        }
+**Length**: ${video.duration} minute(s)
+**Uploaded**: ${video.uploadedAt}
+**Views**: ${video.views.toLocaleString()}`
+      )
+      .setThumbnail(video.author.bestAvatar.url);
+    message.lineReplyNoMention(embed);
+  },
+};
