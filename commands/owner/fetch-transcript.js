4e8157dc3f803c86737faf4a0870cc9fb01127a7
gifaldyazkaa <official.gifaldy@gmail.com>
2021-06-30 14:16:43 +0700

    adding fetch-transcript.js

---
+++ commands/owner/fetch-transcript.js
@@ -0,0 +1,26 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const { fetchTranscript } = require("reconlx");
+
+module.exports = {
+  name: "fetch-transcript",
+  aliases: ["transcript", "tsc"],
+  usage: "",
+  description: "Transcript chat to HTML File",
+  hidden: false,
+  premium: false,
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const amount = args.join(" ");
+    if (message.author.id !== "788260234409672754")
+      if (!amount)
+        return message.lineReply("Please specify a number to transcripted!");
+    fetchTranscript(message, amount).then((data) => {
+      const file = new MessageAttachment(data, "index.html");
+      message.channel.send(file);
+    });
+  },
+};
