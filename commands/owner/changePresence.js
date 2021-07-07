b5c59b47b022cb3f45c65beae5bf5bb5eff9b426
gifaldyazkaa <official.gifaldy@gmail.com>
2021-07-07 15:45:51 +0700

    Modify changePresence.js

--- commands/owner/changePresence.js
+++ commands/owner/changePresence.js
@@ -2,7 +2,7 @@ const { Client, Message, MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "presence",
-  aliases: ["change-status"],
+  aliases: ["change-status", "rpc"],
   usage: "<text>",
   description: "Change Custom Presence Status Text",
   hidden: false,
@@ -16,21 +16,12 @@ module.exports = {
     if (message.author.id !== "788260234409672754") return;
     const text = args.join(" ");
     if (!text) return message.lineReply("Please define a text bro, noob");
-    if (text === "streaming") {
-      client.user.setActivity(`${prefix}help`, {
+      client.user.setActivity(`${text}`, {
         type: "STREAMING",
         url: "https://www.twitch.tv/falcxxr",
       });
       message.lineReplyNoMention(
-        `Successfully Set Presence to Default.\nWith Presence Type : **Streaming**\nStreaming URL : \`https://twitch.tv/falcxxr\``
+        `Successfully Set Presence to => \`${text}\`.\nWith Presence Type : **Streaming**\nStreaming URL : \`https://twitch.tv/falcxxr\` (Default)`
       );
-    } else {
-      client.user.setActivity(`${text}`, {
-        type: "PLAYING",
-      });
-      message.lineReplyNoMention(
-        `Successfully Set Presence to => \`${text}\`.\nWith Presence Type : **Playing**`
-      );
-    }
   },
 };
