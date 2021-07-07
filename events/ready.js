e92fcabe3b534bb60fb478012380d62d44fca1d3
Gifaldy Azka <68645946+gifaldyazkaa@users.noreply.github.com>
2021-07-07 21:11:55 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,23 +4,10 @@ const client = require("../index");
 const prefix = require("../config.json").prefix;
 
 client.on("ready", () => {
-  const arrayOfStatus = [
-    `.help or @Exxyll`,
-    `${client.guilds.cache.size} Servers`,
-    `${client.channels.cache.size} Channels`,
-    `${client.users.cache.size} Users`,
-  ];
-  let indexnum = 0;
-  setInterval(() => {
-    if (indexnum === arrayOfStatus.length) index = 0;
-    const presenceStatus = arrayOfStatus[indexnum];
-    console.log(presenceStatus);
-    client.user.setActivity(presenceStatus, {
+  client.user.setActivity(`.help or @Exxyll | Live on ${client.guilds.cache.size}`, {
       type: "STREAMING",
       url: "https://www.twitch.tv/falcxxr",
-    });
-    indexnum++;
-  }, 15000);
+  }
   console.log(`${client.user.username} has logged on!`);
 
   const clientDetails = {
