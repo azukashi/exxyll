65389e4d0d272b4859001537bfcd69ef1e2518b6
gifaldyazkaa <official.gifaldy@gmail.com>
2021-07-15 16:47:18 +0700

    Updating list-member.js

--- commands/owner/list-member.js
+++ commands/owner/list-member.js
@@ -14,6 +14,8 @@ module.exports = {
     const guild_id = args.join(" ");
     if (!guild_id) return message.lineReply("Specify Guild ID Bro");
     const list = client.guilds.cache.get(guild_id);
-    list.members.cache.each((member) => message.channel.send(member));
+    list.members.cache.each((member) =>
+      message.lineReplyNoMention(`\`\`\`js\n${member}\n\`\`\``)
+    );
   },
 };
