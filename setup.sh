c8850259138c7bfb17c5b4d14a168333adecc01b
gifaldyazkaa <official.gifaldy@gmail.com>
2021-07-24 06:08:57 +0700

    Adding setup shell script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo Checking node version...
+node --version
+echo ""
+echo Checking yarn version...
+yarn --version
+echo ""
+echo Starting installing required modules...
+yarn install
+echo ""
+echo Process is done. Now you can run yarn start!
