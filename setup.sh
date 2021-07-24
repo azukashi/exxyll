7fc8a196a63b3138e20a55302624bf019f5921c0
gifaldyazkaa <official.gifaldy@gmail.com>
2021-07-24 17:52:08 +0700

    Change some files

--- setup.sh
+++ setup.sh
@@ -1,10 +1,22 @@
+# Make sure to install node.js & yarn before running this shit script lmao
+# If you dont have, Go install it first.
+
+# Checking for Installed Node.js Version
 echo Checking node version...
 node --version
 echo ""
+
+# Checking for yarn version. Make sure you have yarn installed.
 echo Checking yarn version...
 yarn --version
 echo ""
+
+# Install required dependecies declared in package.json
 echo Starting installing required modules...
 yarn install
 echo ""
-echo Process is done. Now you can run yarn start!
+
+# Process done. Join support server if you had an error lmao :v
+# Yeah bro, run yarn start. don't do npm start. npm is bad
+echo Process is done. Now you can run
+echo yarn start
