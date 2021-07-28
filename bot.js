a60fb6694f7ae14fe673280c7ff17caf015a0cff
gifaldyazkaa <official.gifaldy@gmail.com>
2021-07-29 05:55:24 +0700

    Add bot-growth command.

--- bot.js
+++ bot.js
@@ -1,10 +1,8 @@
-// This file is used for sharding the bot. If you want
+// This file is used for sharding the bot.
 // Just run `node bot.js` to shard the bot.
-// I'm not sure if this is the best way to do it, but it works.
-// Just do if the bot is over 2000 servers.
+// Just do this if the bot is over 2000 servers.
 
 const { ShardingManager } = require("discord.js");
-const config = require("./config.json");
 const chalk = require("chalk");
 
 const manager = new ShardingManager("./index.js", {
