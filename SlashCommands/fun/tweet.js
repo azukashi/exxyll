9d972d6247a40ee5c334e7e6655e8dcd357fd380
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-01 00:23:01 +0700

    Add tweet slash cmd

---
+++ SlashCommands/fun/tweet.js
@@ -0,0 +1,35 @@
+const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "tweet",
+  description: "Tweet something!",
+  options: [
+    {
+      type: 3,
+      name: "text",
+      description: "Text to tweet",
+      required: true,
+    },
+  ],
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const [text] = args;
+    fetch(
+      `https://nekobot.xyz/api/imagegen?type=tweet&username=${interaction.user.username}&text=${text}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let embed = new MessageEmbed()
+          .setTitle("Tweet!")
+          .setImage(data.message)
+          .setColor("BLUE")
+          .setTimestamp();
+        interaction.followUp({ embeds: [embed] });
+      });
+  },
+};
