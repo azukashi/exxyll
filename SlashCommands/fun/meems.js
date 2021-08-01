2a7aaa2b2f3ed9e3ce002e7b49c54eb2afdf9d26
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-02 06:48:10 +0700

    Add meems.js

---
+++ SlashCommands/fun/meems.js
@@ -0,0 +1,24 @@
+const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "meme",
+  description: "Generates random memes from Reddit",
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const res = await fetch(`http://api.popcatdev.repl.co/meme`);
+    const meme = await res.json();
+    const embed = new MessageEmbed()
+      .setTitle(meme.title)
+      .setURL(meme.url)
+      .setColor("RANDOM")
+      .setImage(meme.image)
+      .setFooter(`👍 ${meme.upvotes} || 💬 ${meme.comments}`);
+
+    interaction.followUp({ embeds: [embed] });
+  },
+};
