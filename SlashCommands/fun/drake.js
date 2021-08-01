36c913f02af4483198d912f3b4c8db9c2cf9ebc6
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-02 06:42:17 +0700

    Bodohnya... Udah ada malah ditambahin

---
+++ SlashCommands/fun/drake.js
@@ -0,0 +1,36 @@
+const { CommandInteraction, Client, MessageAttachment } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "drake",
+  description: "Generates a Drake Memes",
+  options: [
+    {
+      type: 3,
+      name: "text_1",
+      description: "First Argument",
+      required: true,
+    },
+    {
+      type: 3,
+      name: "text_2",
+      description: "Second Argument",
+      required: true,
+    },
+  ],
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const [text_1, text_2] = args;
+    const res = await fetch(
+      `https://frenchnoodles.xyz/api/endpoints/drake/?text1=${text_1}&text2=${text_2}`,
+      {}
+    );
+    let Image = await res.buffer();
+    const drakememe = new MessageAttachment(Image);
+    interaction.followUp({ files: [drakememe] });
+  },
+};
