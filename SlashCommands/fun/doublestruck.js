973785d7d8686a7a00ca7126a5322ab6323a5b17
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-02 06:55:00 +0700

    Add doublestruck.js

---
+++ SlashCommands/fun/doublestruck.js
@@ -0,0 +1,28 @@
+const { CommandInteraction, Client, WebhookClient } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "doublestruck",
+  description: "Doublestruck a text",
+  options: [
+    {
+      type: 3,
+      name: "text",
+      description: "Text to doublestruck",
+      required: true,
+    },
+  ],
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const [text] = args;
+    fetch(`https://api.popcatdev.repl.co/doublestruck?text=${text}`)
+      .then((res) => res.json())
+      .then((body) => {
+        interaction.followUp({ content: body.text });
+      });
+  },
+};
