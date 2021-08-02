2cb68db883da6f0ff303e9706609bee53868e245
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-02 07:12:27 +0700

    Add discord.js docs

---
+++ SlashCommands/info/djs-docs.js
@@ -0,0 +1,32 @@
+const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "djs",
+  description: "Search for a Class, Properties at Discord.js v13",
+  options: [
+    {
+      type: 3,
+      name: "query",
+      description: "Text to search. Class or Properties or something",
+      required: true,
+    },
+  ],
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const [query] = args;
+    const url = `https://djsdocs.sorta.moe/v2/embed?src=master&q=${encodeURIComponent(
+      query
+    )}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((body) => {
+        console.log(body);
+        interaction.followUp({ embeds: [body] });
+      });
+  },
+};
