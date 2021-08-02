75ccce4a5fd1ddb2207e6b8ba12856b2cb868ff2
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-02 14:53:17 +0700

    Add `avatar.js`

---
+++ SlashCommands/user/avatar.js
@@ -0,0 +1,40 @@
+const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "avatar",
+  description: "Shows user avatar",
+  options: [
+    {
+      type: 6,
+      name: "user",
+      description: "User to show avatar. This is optional.",
+      required: false,
+    },
+  ],
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    let [user] = args;
+    if (!user) user = interaction.user.id;
+    // Convert uid to advanced properties by using Client
+    const fixUser = client.users.cache.get(user);
+    const pngFormat = fixUser.displayAvatarURL({ format: "png" });
+    const jpgFormat = fixUser.displayAvatarURL({ format: "jpg" });
+    const webpFormat = fixUser.displayAvatarURL();
+    const avatar = fixUser.displayAvatarURL({
+      dynamic: true,
+      size: 512,
+    });
+    const embed = new MessageEmbed()
+      .setTitle(`${fixUser.username}'s avatar`)
+      .setDescription(
+        `[PNG](${pngFormat}) | [JPG](${jpgFormat}) | [WEBP](${webpFormat})`
+      )
+      .setImage(avatar)
+      .setColor("BLUE");
+    interaction.followUp({ embeds: [embed] });
+  },
+};
