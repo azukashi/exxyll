3d559683700a3420d04a69ab8e4fa29d1bbd7f5f
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-04 19:07:04 +0700

    Balik lagi ke last commit-nya om bayu

--- events/welcomer.js
+++ events/welcomer.js
@@ -1,18 +1,19 @@
 const Schema = require("../models/welcomeChannel.js");
-const { MessageEmbed, MessageAttachment } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = (client) => {
-  client.on("guildMemberAdd", async (user, member) => {
+  client.on("guildMemberAdd", async (member) => {
     Schema.findOne({ Guild: member.guild.id }, async (err, data) => {
       if (!data) return;
 
       const channel = member.guild.channels.cache.get(data.Channel);
+      // const guildAvatar = client.guilds.cache.get(message.guild.id).iconURL({ dynamic: true });
 
       channel.send(
         new MessageEmbed()
           .setTitle("New Member!")
           .setDescription(
-            `Hey <@${member.user.id}>, Welcome to **${member.guild.name}!**`
+            `Hey <@${member.user.id}>, Welcome to **${member.guild.name}**`
           )
           .setFooter(`${member.guild.memberCount} members`)
           .setColor("BLUE")
