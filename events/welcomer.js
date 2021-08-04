e9206789177c517f8c86af25399452a8b7e66d4b
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-04 18:43:23 +0700

    Some change with events/welcomer.js

--- events/welcomer.js
+++ events/welcomer.js
@@ -1,24 +1,37 @@
 const Schema = require("../models/welcomeChannel.js");
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageAttachment } = require("discord.js");
 
 module.exports = (client) => {
-  client.on("guildMemberAdd", async (member) => {
+  client.on("guildMemberAdd", async (user, guild, member) => {
     Schema.findOne({ Guild: member.guild.id }, async (err, data) => {
       if (!data) return;
 
       const channel = member.guild.channels.cache.get(data.Channel);
-      // const guildAvatar = client.guilds.cache.get(message.guild.id).iconURL({ dynamic: true });
+      let base = "https://luminabot.xyz/api/image/welcomecard2?";
+      let image =
+        base +
+        `avatar=${user.user.displayAvatarURL({
+          dynamic: false,
+          format: "png",
+        })}&username=${user.user.username}&membercount=${
+          user.guild.memberCount
+        }&guildname=${user.guild.name}`;
+      let att = new MessageAttachment(image, "welcome.png");
 
       channel.send(
         new MessageEmbed()
           .setTitle("New Member!")
+          .setThumbnail(
+            user.user.displayAvatarURL({ size: 512, dynamic: true })
+          )
           .setDescription(
-            `Hey <@${member.user.id}>, Welcome to **${member.guild.name}**`
+            `Hey <@${member.user.id}>, Welcome to **${member.guild.name}!**`
           )
           .setFooter(`${member.guild.memberCount} members`)
           .setColor("BLUE")
           .setTimestamp()
       );
+      channel.send(att);
     });
   });
 };
