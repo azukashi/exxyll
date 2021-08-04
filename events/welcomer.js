ea81047b828da63ae7dd01153dbe01d79a2b8f9c
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-04 18:56:00 +0700

    Udah stuck. Nga bisa di-apa apa in lagi

--- events/welcomer.js
+++ events/welcomer.js
@@ -7,23 +7,10 @@ module.exports = (client) => {
       if (!data) return;
 
       const channel = member.guild.channels.cache.get(data.Channel);
-      let base = "https://luminabot.xyz/api/image/welcomecard2?";
-      let image =
-        base +
-        `avatar=${user.user.displayAvatarURL({
-          dynamic: false,
-          format: "png",
-        })}&username=${user.user.username}&membercount=${
-          user.guild.memberCount
-        }&guildname=${user.guild.name}`;
-      let att = new MessageAttachment(image, "welcome.png");
 
       channel.send(
         new MessageEmbed()
           .setTitle("New Member!")
-          .setThumbnail(
-            user.user.displayAvatarURL({ size: 512, dynamic: true })
-          )
           .setDescription(
             `Hey <@${member.user.id}>, Welcome to **${member.guild.name}!**`
           )
@@ -31,7 +18,6 @@ module.exports = (client) => {
           .setColor("BLUE")
           .setTimestamp()
       );
-      channel.send(att);
     });
   });
 };
