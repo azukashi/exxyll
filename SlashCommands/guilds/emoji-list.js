e0ebe009ba0e2400a534642d0b48f5940daa5fa2
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-07 08:07:01 +0700

    Add `emoji-list.js`

---
+++ SlashCommands/guilds/emoji-list.js
@@ -0,0 +1,56 @@
+const { CommandInteraction, Client, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'emojilist',
+    description: 'Displays every emoji of the guild, Where the command is used',
+    aliases: [],
+    /** 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     * @param {String[]} args 
+     */
+    run: async(client, interaction, args) => {
+        try {
+            let Emojis = "";
+            let EmojisAnimated = "";
+            let EmojiCount = 0;
+            let Animated = 0;
+            let OverallEmojis = 0;
+            function Emoji(id) {
+              return client.emojis.cache.get(id).toString();
+            }
+            interaction.guild.emojis.cache.forEach((emoji) => {
+              OverallEmojis++;
+              if (emoji.animated) {
+                Animated++;
+                EmojisAnimated += Emoji(emoji.id);
+              } else {
+                EmojiCount++;
+                Emojis += Emoji(emoji.id);
+              }
+            });
+            let emn = new MessageEmbed();
+            emn.setTitle(
+              `<:add_reaction:863214931599818783> Showing Emojis of ${interaction.guild.name}`
+            );
+            emn.setThumbnail(
+              interaction.guild.iconURL({ dynamic: true, format: "png", size: 512 })
+            );
+            emn.setDescription(
+              `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}`
+            );
+            emn
+              .setColor("BLUE")
+              .setFooter(
+                `Emoji Lists | Command Request by ${interaction.user.tag}`,
+                interaction.user.displayAvatarURL({ dynamic: true })
+              );
+            interaction.followUp({ embeds: [emn] });
+          } catch (err) {
+            interaction.followUp(
+              "Oops! Looks like something went wrong, Please try again Later."
+            );
+            console.log(err)
+          }
+    }
+}
