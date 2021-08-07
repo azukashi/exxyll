30662c2d2a7f59e6b5723d61ab6e8d6c38d5d650
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-07 08:06:44 +0700

    Add `channel-info.js`

---
+++ SlashCommands/guilds/channel-info.js
@@ -0,0 +1,47 @@
+const { CommandInteraction, Client, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'channelinfo',
+    description: 'Returns Information aboout Channels',
+    options: [
+        {
+            type: 7,
+            name: 'channel',
+            description: 'Channel to show information about',
+            required: true
+        }
+    ],
+    /** 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     * @param {String[]} args 
+     */
+    run: async(client, interaction, args) => {
+        function checkDays(date) {
+            let now = new Date();
+            let diff = now.getTime() - date.getTime();
+            let days = Math.floor(diff / 86400000);
+            return days + (days == 1 ? " day" : " days") + " ago";
+          }
+          let [channel] = args;
+          let fixedChannel = client.channels.cache.get(channel);
+          let inline = true;
+          try {
+            let e = new MessageEmbed()
+              .setTitle(`<:channel:863363700463632414> Channel Information`)
+              .setThumbnail(interaction.guild.iconURL({ dynamic: false }))
+              .setDescription(`Information About ${fixedChannel}`)
+              .addField("Created At:", `${checkDays(fixedChannel.createdAt)}`, inline)
+              .addField("Channel ID:", `${fixedChannel.id}`, inline)
+              .addField("Channel Type:", `${fixedChannel.type}`, inline)
+              .setFooter(
+                `Channel Information | Command Request by ${interaction.user.tag}`,
+                interaction.user.displayAvatarURL({ dynamic: true })
+              )
+              .setColor("BLUE");
+            interaction.followUp({ embeds: [e] });
+          } catch (error) {
+            interaction.followUp(error);
+          }
+    }
+}
