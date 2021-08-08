9f509314724e4d98e9a6f18795daec7a94e75442
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-08 12:07:38 +0700

    Add `nuke-channel.js`

---
+++ SlashCommands/guilds/nuke-channel.js
@@ -0,0 +1,28 @@
+const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "nuke",
+  description: "Nuke current channel",
+  /**
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    interaction.channel.clone().then((ch) => {
+      ch.setParent(interaction.channel.parent.id);
+      ch.setPosition(interaction.channel.position);
+      interaction.channel.delete();
+
+      const embed = new MessageEmbed()
+        .setTitle("This channel has been nuked!")
+        .setImage(
+          "https://tenor.com/view/explosion-mushroom-cloud-atomic-bomb-bomb-boom-gif-4464831"
+        )
+        .setColor("RED")
+        .setFooter(`Action performed by ${interaction.user.tag}`);
+
+      ch.send({ embeds: [embed] }).then((m) => m.delete({ timeout: 7000 }));
+    });
+  },
+};
