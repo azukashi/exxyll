be9cdf3d248d7631ceff4c37bec405e789f815f2
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-08 17:30:38 +0700

    Update and add Events for Buttons

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,3 +1,4 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 const client = require("../index");
 
 client.on("interactionCreate", async (interaction) => {
@@ -17,5 +18,63 @@ client.on("interactionCreate", async (interaction) => {
     });
 
     cmd.run(client, interaction, args);
+  }
+  if (interaction.customId === "tic") {
+    try {
+      const thread = await interaction.channel.threads
+        .create({
+          name: `${interaction.user.username}#${interaction.user.discriminator}`,
+          autoArchiveDuration: 60,
+          // type: 'GUILD_PRIVATE_THREAD',
+          reason: "New Ticket has been created!",
+        })
+        .catch((err) => {
+          const errormsg = new MessageEmbed()
+            .setTitle("Something went wrong!")
+            .setDescription(`Given error message : \n\`\`\`yml\n${err}\n\`\`\``)
+            .setColor("RED")
+            .setTimestamp();
+          return interaction.followUp({ embeds: [errormsg], ephemeral: true });
+        });
+      await thread.setLocked(true).catch((err) => {
+        console.log(err);
+      });
+      const embed = new MessageEmbed()
+        .setTitle("Ticket")
+        .setDescription(
+          "Hello there, \nThe staff will be here as soon as possible mean while tell us about your issue!\nThank You!"
+        )
+        .setColor("GREEN")
+        .setTimestamp()
+        .setAuthor(
+          interaction.guild.name,
+          interaction.guild.iconURL({
+            dynamic: true,
+          })
+        );
+
+      const del = new MessageActionRow().addComponents(
+        new MessageButton()
+          .setCustomId("del")
+          .setLabel("🗑️ Delete Ticket!")
+          .setStyle("DANGER")
+      );
+      interaction.user.send("Your ticket has been opened!");
+      thread
+        .send({
+          content: `Welcome <@${interaction.user.id}>`,
+          embeds: [embed],
+          components: [del],
+        })
+      console.log(`Created thread: ${thread.name}`);
+      setTimeout(() => {
+        interaction.channel.bulkDelete(1);
+      }, 5000);
+    } catch (err) {
+      console.log(err);
+    }
+  } else if (interaction.customId === "del") {
+    const thread = interaction.channel;
+    thread.delete();
   }
 });
