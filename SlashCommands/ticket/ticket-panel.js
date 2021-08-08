d86502a32493dc55cf6704caba8c9a0f86060b4c
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-08 17:30:04 +0700

    Adding ticket commands to Slash Folder

---
+++ SlashCommands/ticket/ticket-panel.js
@@ -0,0 +1,42 @@
+const { CommandInteraction, Client, MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
+
+module.exports = {
+    name: 'ticket',
+    description: 'Create a ticket',
+    /** 
+     * @param {Client} client 
+     * @param {CommandInteraction} interaction 
+     * @param {String[]} args 
+     */
+    run: async(client, interaction, args) => {
+        const embed = new MessageEmbed()
+            .setColor('BLUE')
+            .setAuthor(interaction.guild.name, interaction.guild.iconURL({
+                dynamic: true
+            }))
+            .setDescription(
+                "__**How to make a ticket**__\n" +
+
+
+                "> Click on the reaction that relates to your need\n" +
+
+                "> Once the ticket is made you will be able to type in there"
+
+            )
+            .setTitle('Tickets')
+
+
+        const bt = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                .setCustomId('tic')
+                .setLabel('🎫 Create Ticket!')
+                .setStyle('PRIMARY'),
+            );
+
+        interaction.followUp({
+            embeds: [embed],
+            components: [bt]
+        });
+    }
+}
