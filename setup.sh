8d158d85e4bd46d892b70a9844a0a3af1e5df509
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-09 16:09:54 +0700

    Add Setup Script

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Make sure to install node.js & yarn before running this shit script lmao
+# If you dont have, Go install it first.
+
+# Clear the screen first.
+clear
+
+# Checking directory
+echo Checking directory list...
+ls -lR
+echo ""
+clear
+
+# Checking for Installed Node.js Version
+echo Checking node version...
+node --version
+echo ""
+
+# Checking for yarn version. Make sure you have yarn installed.
+echo Checking yarn version...
+yarn --version
+echo ""
+
+# Install required dependecies declared in package.json
+echo Starting installing required modules...
+yarn install
+echo ""
+
+# Clear the screen after installing all deps
+clear
+
+# Process done. Join support server if you had an error lmao :v
+# Yeah bro, run yarn start. don't do npm start. npm is bad
+echo Process is done. Now, create .env and run
+echo yarn start
+echo ""
+echo ""
+echo "If you don't know what value you must add at .env, Read the docs at"
+echo "https://github.com/gifaldyazkaa/exxyll-origin"
+echo ""
+echo "Have an issues? Create new one at exxyll-origin repo!"
+echo "https://github.com/gifaldyazkaa/exxyll-origin/issues"
+echo ""
