89aeaa403a166b0b726a5899e849b340c0d903ce
gifaldyazkaa <official.gifaldy@gmail.com>
2021-08-12 15:36:45 +0700

    Add config to Normal Commands

---
+++ commands/config/prefix.js
@@ -0,0 +1,42 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const prefixSchema = require("../../models/prefix");
+
+module.exports = {
+  name: "set-prefix",
+  usage: "<new-prefix>",
+  description: "Set Custom Prefixes",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    if (!message.member.permissions.has("MANAGE_GUILD"))
+      return message.reply(
+        "You need `MANAGE_GUILD` Permission in order to run this command!"
+      );
+    const res = await args.join(" ");
+    if (!res) return message.lineReply("Please specify a prefix to change to.");
+    prefixSchema.findOne({ Guild: message.guild.id }, async (err, data) => {
+      if (err) throw err;
+      if (data) {
+        prefixSchema.findOneAndDelete({ Guild: message.guild.id });
+        data = new prefixSchema({
+          Guild: message.guild.id,
+          Prefix: res,
+        });
+        data.save();
+        message.channel.send(`Prefix has been updated to **${res}**`);
+      } else {
+        data = new prefixSchema({
+          Guild: message.guild.id,
+          Prefix: res,
+        });
+        data.save();
+        message.channel.send(
+          `Custom prefix in this server is now set to **${res}**`
+        );
+      }
+    });
+  },
+};
---
+++ commands/config/reset-prefix.js
@@ -0,0 +1,21 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const prefixSchema = require("../../models/prefix");
+const prefix = require("../../config.json").prefix;
+
+module.exports = {
+  name: "reset-prefix",
+  description: "Reset Customized Prefix to Default.",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    if (!message.member.permissions.has("MANAGE_GUILD"))
+      return message.reply(
+        "You need `MANAGE_GUILD` Permission in order to run this command!"
+      );
+    await prefixSchema.findOneAndDelete({ Guild: message.guild.id });
+    message.channel.send(`The prefix has been reset to **${prefix}**`);
+  },
+};
