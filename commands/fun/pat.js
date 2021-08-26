97cbfe677586a32b0f8d8cf759b099f497ff5fb3
Gifaldy Azka <official.gifaldy@gmail.com>
2021-08-26 09:55:22 +0700

    Remove user checking

--- commands/fun/pat.js
+++ commands/fun/pat.js
@@ -14,11 +14,6 @@ module.exports = {
   run: async (client, message, args) => {
     const user = message.mentions.users.first();
     if (!user) return message.reply("Please mention a user!");
-    if (user == message.author.id)
-      return message.reply(`No, U can't hug yourself.`);
-    if (user == client.user.id) return message.reply(`No, U can't hug me.`);
-    if (user == message.author.bot)
-      return message.reply(`No, U can't hug bots.`);
     fetch("https://api.waifu.pics/sfw/pat")
       .then((res) => res.json())
       .then((body) => {
