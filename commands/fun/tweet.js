b9ae92b08a4dd5cf99ffedeb755e400f36660671
Gifaldy Azka <official.gifaldy@gmail.com>
2021-08-26 09:52:22 +0700

    Add tweet.js

---
+++ commands/fun/tweet.js
@@ -0,0 +1,30 @@
+const { Client, Message, MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "tweet",
+  description: "Tweet something!",
+  aliases: ["twitter"],
+  emoji: "<:twitter:880275464982245417>",
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const text = args.join(" ");
+    if (!text) return message.reply("Please specify a text!");
+    fetch(
+      `https://nekobot.xyz/api/imagegen?type=tweet&username=${message.author.username}&text=${text}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let embed = new MessageEmbed()
+          .setTitle("Tweet!")
+          .setImage(data.message)
+          .setColor("BLUE")
+          .setTimestamp();
+        message.channel.send({ embeds: [embed] });
+      });
+  },
+};
