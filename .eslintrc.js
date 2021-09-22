2e082c6c2b47374ae6c8cd4d0c0d292be5e98a23
Gifaldy Azka <official.gifaldy@gmail.com>
2021-09-22 13:25:48 +0700

    chore(v2): add ignorePatterns to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  ignorePatterns: ['/commands/info/*', '/SlashCommands/info/*'],
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
 };
