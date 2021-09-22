47701db85d30a7d71dd591e5c9d40f7651c9a315
Gifaldy Azka <official.gifaldy@gmail.com>
2021-09-22 13:26:53 +0700

    chore(v2): wrap help command in try-catch block

--- SlashCommands/info/help.js
+++ SlashCommands/info/help.js
@@ -12,93 +12,99 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, interaction, args) => {
-    const emojis = {
-      config: "<:settings:863951175875559425>",
-      developer: "<:developer:855302873785040897>",
-      fun: "<:teri_funny:876637356986343440>",
-      guild: "<:partnernew:863214932585873438>",
-      info: "<:wininfo:875298951362932736>",
-      levelling: "⏫",
-      music: "🎵",
-    };
-    const directories = [...new Set(client.commands.map((cmd) => cmd.directory))];
-
-    const formatString = (str) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
-
-    const categories = directories.map((dir) => {
-      const getCommands = client.commands
-        .filter((cmd) => cmd.directory === dir)
-        .map((cmd) => {
-          return {
-            name: cmd.name || "No Name",
-            description: cmd.description || "No Description Provided",
-            emoji: cmd.emoji || "",
-          };
-        });
-
-      return {
-        directory: formatString(dir),
-        commands: getCommands,
+    try {
+      const emojis = {
+        config: "<:settings:863951175875559425>",
+        developer: "<:developer:855302873785040897>",
+        fun: "<:teri_funny:876637356986343440>",
+        games: "🎮",
+        guild: "<:partnernew:863214932585873438>",
+        info: "<:wininfo:875298951362932736>",
+        levelling: "⏫",
+        user: "👤",
       };
-    });
-
-    const embed = new MessageEmbed()
-      .setTitle("Exxyll Help Desk")
-      .setThumbnail(client.user.displayAvatarURL({ size: 512 }))
-      .setDescription("Please choose a category in the dropdown menu!\n\nFor information about usage, or something about commands, You can [Read the Docs](https://google.com) for more info!")
-      .setColor("BLUE");
-
-    const components = (state) => [
-      new MessageActionRow().addComponents(
-        new MessageSelectMenu()
-          .setCustomId("help-menu")
-          .setPlaceholder("Please select a category")
-          .setDisabled(state)
-          .addOptions(
-            categories.map((cmd) => {
+      const directories = [...new Set(client.commands.map((cmd) => cmd.directory))];
+  
+      const formatString = (str) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+  
+      const categories = directories.map((dir) => {
+        const getCommands = client.commands
+          .filter((cmd) => cmd.directory === dir)
+          .map((cmd) => {
+            return {
+              name: cmd.name || "No Name",
+              description: cmd.description || "No Description Provided",
+              emoji: cmd.emoji || "",
+            };
+          });
+  
+        return {
+          directory: formatString(dir),
+          commands: getCommands,
+        };
+      });
+  
+      const embed = new MessageEmbed()
+        .setTitle("Exxyll Help Desk")
+        .setThumbnail(client.user.displayAvatarURL({ size: 512 }))
+        .setDescription("Please choose a category in the dropdown menu!\n\nFor information about usage, or something about commands, You can [Read the Docs](https://google.com) for more info!")
+        .setColor("BLUE");
+  
+      const components = (state) => [
+        new MessageActionRow().addComponents(
+          new MessageSelectMenu()
+            .setCustomId("help-menu")
+            .setPlaceholder("Please select a category")
+            .setDisabled(state)
+            .addOptions(
+              categories.map((cmd) => {
+                return {
+                  label: cmd.directory,
+                  value: cmd.directory.toLowerCase(),
+                  description: `Commands from ${cmd.directory} category`,
+                  emoji: emojis[cmd.directory.toLowerCase()] || null,
+                };
+              })
+            )
+        ),
+      ];
+  
+      const initialMessage = await interaction.followUp({
+        embeds: [embed],
+        components: components(false),
+      });
+  
+      const filter = (interaction) => interaction.user.id === interaction.user.id;
+  
+      const collector = interaction.channel.createMessageComponentCollector({
+        filter,
+        componentType: "SELECT_MENU",
+      });
+  
+      collector.on("collect", (interaction) => {
+        const [directory] = interaction.values;
+        const category = categories.find((x) => x.directory.toLowerCase() === directory);
+  
+        const categoryEmbed = new MessageEmbed()
+          .setTitle(`${emojis[directory.toLowerCase()]} ${formatString(directory)} Commands`)
+          .setDescription(`Here are the list of commands!`)
+          .setColor("BLUE")
+          .addFields(
+            category.commands.map((cmd) => {
               return {
-                label: cmd.directory,
-                value: cmd.directory.toLowerCase(),
-                description: `Commands from ${cmd.directory} category`,
-                emoji: emojis[cmd.directory.toLowerCase()] || null,
+                name: `${cmd.emoji} \`${cmd.name}\``,
+                value: cmd.description,
+                inline: true,
               };
             })
           )
-      ),
-    ];
-
-    const initialMessage = await interaction.followUp({
-      embeds: [embed],
-      components: components(false),
-    });
-
-    const filter = (interaction) => interaction.user.id === interaction.user.id;
-
-    const collector = interaction.channel.createMessageComponentCollector({
-      filter,
-      componentType: "SELECT_MENU",
-    });
-
-    collector.on("collect", (interaction) => {
-      const [directory] = interaction.values;
-      const category = categories.find((x) => x.directory.toLowerCase() === directory);
-
-      const categoryEmbed = new MessageEmbed()
-        .setTitle(`${emojis[directory.toLowerCase()]} ${formatString(directory)} Commands`)
-        .setDescription(`Here are the list of commands!`)
-        .setColor("BLUE")
-        .addFields(
-          category.commands.map((cmd) => {
-            return {
-              name: `${cmd.emoji} \`${cmd.name}\``,
-              value: cmd.description,
-              inline: true,
-            };
-          })
-        )
-        .setTimestamp();
-
-      interaction.update({ embeds: [categoryEmbed] });
-    });
+          .setTimestamp();
+  
+        interaction.update({ embeds: [categoryEmbed] });
+      });
+    } catch (err) {
+      interaction.followUp({ content: 'Uh oh! Something unexcepted. Try to running command again!', ephemeral: true });
+      console.log(err);
+    }
   },
 };
--- commands/info/help.js
+++ commands/info/help.js
@@ -13,94 +13,100 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    const emojis = {
-      config: "<:settings:863951175875559425>",
-      developer: "<:developer:855302873785040897>",
-      fun: "<:teriFunny:876637356986343440>",
-      guild: "<:partnernew:863214932585873438>",
-      info: "<:wininfo:875298951362932736>",
-      levelling: "⏫",
-      music: "🎵",
-    };
-    const directories = [...new Set(client.commands.map((cmd) => cmd.directory))];
-
-    const formatString = (str) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
-
-    const categories = directories.map((dir) => {
-      const getCommands = client.commands
-        .filter((cmd) => cmd.directory === dir)
-        .map((cmd) => {
-          return {
-            name: cmd.name || "No Name",
-            description: cmd.description || "No Description Provided",
-            emoji: cmd.emoji || "",
-          };
-        });
-
-      return {
-        directory: formatString(dir),
-        commands: getCommands,
+    try {
+      const emojis = {
+        config: "<:settings:863951175875559425>",
+        developer: "<:developer:855302873785040897>",
+        fun: "<:teriFunny:876637356986343440>",
+        games: "🎮",
+        guild: "<:partnernew:863214932585873438>",
+        info: "<:wininfo:875298951362932736>",
+        levelling: "⏫",
+        user: "👤",
       };
-    });
-
-    const embed = new MessageEmbed()
-      .setTitle("Exxyll Help Desk")
-      .setThumbnail(client.user.displayAvatarURL({ size: 512 }))
-      .setDescription("Please choose a category in the dropdown menu!\n\nFor information about usage, or something about commands, You can [Read the Docs](https://google.com) for more info!")
-      .setColor("BLUE");
-
-    const components = (state) => [
-      new MessageActionRow().addComponents(
-        new MessageSelectMenu()
-          .setCustomId("help-menu")
-          .setPlaceholder("Please select a category")
-          .setDisabled(state)
-          .addOptions(
-            categories.map((cmd) => {
+      const directories = [...new Set(client.commands.map((cmd) => cmd.directory))];
+  
+      const formatString = (str) => `${str[0].toUpperCase()}${str.slice(1).toLowerCase()}`;
+  
+      const categories = directories.map((dir) => {
+        const getCommands = client.commands
+          .filter((cmd) => cmd.directory === dir)
+          .map((cmd) => {
+            return {
+              name: cmd.name || "No Name",
+              description: cmd.description || "No Description Provided",
+              emoji: cmd.emoji || "",
+            };
+          });
+  
+        return {
+          directory: formatString(dir),
+          commands: getCommands,
+        };
+      });
+  
+      const embed = new MessageEmbed()
+        .setTitle("Exxyll Help Desk")
+        .setThumbnail(client.user.displayAvatarURL({ size: 512 }))
+        .setDescription("Please choose a category in the dropdown menu!\n\nFor information about usage, or something about commands, You can [Read the Docs](https://google.com) for more info!")
+        .setColor("BLUE");
+  
+      const components = (state) => [
+        new MessageActionRow().addComponents(
+          new MessageSelectMenu()
+            .setCustomId("help-menu")
+            .setPlaceholder("Please select a category")
+            .setDisabled(state)
+            .addOptions(
+              categories.map((cmd) => {
+                return {
+                  label: cmd.directory,
+                  value: cmd.directory.toLowerCase(),
+                  description: `Commands from ${cmd.directory} category`,
+                  emoji: emojis[cmd.directory.toLowerCase()] || null,
+                };
+              })
+            )
+        ),
+      ];
+  
+      const initialMessage = await message.channel.send({
+        embeds: [embed],
+        components: components(false),
+      });
+  
+      const filter = (interaction) => interaction.user.id === message.author.id;
+  
+      const collector = message.channel.createMessageComponentCollector({
+        filter,
+        componentType: "SELECT_MENU",
+      });
+  
+      collector.on("collect", (interaction) => {
+        const [directory] = interaction.values;
+        const category = categories.find((x) => x.directory.toLowerCase() === directory);
+  
+        const categoryEmbed = new MessageEmbed()
+          .setTitle(`${emojis[directory.toLowerCase()]} ${formatString(directory)} Commands`)
+          .setThumbnail(client.user.displayAvatarURL({ dynamic: 512 }))
+          .setDescription(`Here are the list of commands!`)
+          .setColor("BLUE")
+          .addFields(
+            category.commands.map((cmd) => {
               return {
-                label: cmd.directory,
-                value: cmd.directory.toLowerCase(),
-                description: `Commands from ${cmd.directory} category`,
-                emoji: emojis[cmd.directory.toLowerCase()] || null,
+                name: `${cmd.emoji} \`${cmd.name}\``,
+                value: cmd.description,
+                inline: true,
               };
             })
           )
-      ),
-    ];
-
-    const initialMessage = await message.channel.send({
-      embeds: [embed],
-      components: components(false),
-    });
-
-    const filter = (interaction) => interaction.user.id === message.author.id;
-
-    const collector = message.channel.createMessageComponentCollector({
-      filter,
-      componentType: "SELECT_MENU",
-    });
-
-    collector.on("collect", (interaction) => {
-      const [directory] = interaction.values;
-      const category = categories.find((x) => x.directory.toLowerCase() === directory);
-
-      const categoryEmbed = new MessageEmbed()
-        .setTitle(`${emojis[directory.toLowerCase()]} ${formatString(directory)} Commands`)
-        .setThumbnail(client.user.displayAvatarURL({ dynamic: 512 }))
-        .setDescription(`Here are the list of commands!`)
-        .setColor("BLUE")
-        .addFields(
-          category.commands.map((cmd) => {
-            return {
-              name: `${cmd.emoji} \`${cmd.name}\``,
-              value: cmd.description,
-              inline: true,
-            };
-          })
-        )
-        .setTimestamp();
-
-      interaction.update({ embeds: [categoryEmbed] });
-    });
+          .setTimestamp();
+  
+        interaction.update({ embeds: [categoryEmbed] });
+      });
+    } catch (err) {
+      console.log(err);
+      message.channel.send({ content: 'Uh oh! Something unexcepted. Try to running command again!' });
+    }
   },
 };
