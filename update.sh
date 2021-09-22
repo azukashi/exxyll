62ebb2257879b4efca375a7976000a7734561d70
Gifaldy Azka <official.gifaldy@gmail.com>
2021-09-22 12:21:17 +0700

    chore(v2): remove update script

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# A Linux Shell Script to Update Dependecies with any errors 100% no root
-# I know this is useless
-
-echo "Starting to Update Dependecies..."
-yarn upgrade
-echo ""
-echo "Done."
