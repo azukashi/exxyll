fdbf05d0296901788395474d4fdcdc8df78478e8
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-24 15:25:56 +0700

    fix(v2): fix mongodb connect method

--- handler/mongoose.js
+++ handler/mongoose.js
@@ -1,18 +1,14 @@
-const { mongooseConnectionString } = require("../config.json");
-const mongoose = require("mongoose");
-const chalk = require("chalk");
+const { mongooseConnectionString } = require('../config.json');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
 
-module.exports = () => {
-  if (!mongooseConnectionString) return;
+if (!mongooseConnectionString) return;
 
-  mongoose
-    .connect(mongooseConnectionString, {
-      useFindAndModify: true,
-      useUnifiedTopology: true,
-    })
-    .then(
-      console.log(chalk.greenBright.bold("MongoDB")) +
-        " : " +
-        chalk.whiteBright.bold("Connected to the database!")
-    );
-};
+mongoose
+  .connect(mongooseConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    console.log(chalk.greenBright.bold('MongoDB : Connected to the database!'))
+  );
