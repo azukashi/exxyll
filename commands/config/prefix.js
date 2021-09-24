02297edcc89ea8a2ecd2a4fd0b571e44623f5193
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-24 15:27:10 +0700

    chore(v2): remove user perms checking

--- commands/config/prefix.js
+++ commands/config/prefix.js
@@ -1,24 +1,20 @@
-const { Client, Message, MessageEmbed } = require("discord.js");
-const prefixSchema = require("../../models/prefix");
+const { Client, Message, MessageEmbed } = require('discord.js');
+const prefixSchema = require('../../models/prefix');
 
 module.exports = {
-  name: "set-prefix",
-  description: "Set Custom Prefixes",
-  emoji: "➕",
-  userperm: ["MANAGE_GUILD"],
-  botperm: ["MANAGE_GUILD"],
+  name: 'set-prefix',
+  description: 'Set Custom Prefixes',
+  emoji: '➕',
+  userperm: ['MANAGE_GUILD'],
+  botperm: ['MANAGE_GUILD'],
   /**
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    if (!message.member.permissions.has("MANAGE_GUILD"))
-      return message.reply(
-        "You need `MANAGE_GUILD` Permission in order to run this command!"
-      );
-    const res = await args.join(" ");
-    if (!res) return message.lineReply("Please specify a prefix to change to.");
+    const res = await args.join(' ');
+    if (!res) return message.reply('Please specify a prefix to change to.');
     prefixSchema.findOne({ Guild: message.guild.id }, async (err, data) => {
       if (err) throw err;
       if (data) {
--- commands/config/reset-prefix.js
+++ commands/config/reset-prefix.js
@@ -1,23 +1,19 @@
-const { Client, Message, MessageEmbed } = require("discord.js");
-const prefixSchema = require("../../models/prefix");
-const prefix = require("../../config.json").prefix;
+const { Client, Message, MessageEmbed } = require('discord.js');
+const prefixSchema = require('../../models/prefix');
+const prefix = require('../../config.json').prefix;
 
 module.exports = {
-  name: "reset-prefix",
-  description: "Reset Customized Prefix to Default.",
-  emoji: "🗑️",
-  userperm: ["MANAGE_GUILD"],
-  botperm: ["MANAGE_GUILD"],
+  name: 'reset-prefix',
+  description: 'Reset Customized Prefix to Default.',
+  emoji: '🗑️',
+  userperm: ['MANAGE_GUILD'],
+  botperm: ['MANAGE_GUILD'],
   /**
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    if (!message.member.permissions.has("MANAGE_GUILD"))
-      return message.reply(
-        "You need `MANAGE_GUILD` Permission in order to run this command!"
-      );
     await prefixSchema.findOneAndDelete({ Guild: message.guild.id });
     message.channel.send(`The prefix has been reset to **${prefix}**`);
   },
