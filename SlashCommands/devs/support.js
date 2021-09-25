9e7aa6b5945bbb6f5fe79e5546877db746778657
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-25 07:09:34 +0700

    chore(v2): set suggest to die and completing support cmd

--- SlashCommands/devs/support.js
+++ SlashCommands/devs/support.js
@@ -1,10 +1,10 @@
-const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const { CommandInteraction, Client, MessageEmbed } = require('discord.js');
 
 module.exports = {
-  name: "support",
-  description: "Support & Donate to the Chef at the back-screen of Exxyll!",
-  userperm: "SEND_MESSAGES",
-  botperm: "SEND_MESSAGES",
+  name: 'support',
+  description: 'Support & Donate to the Chef at the back-screen of Exxyll!',
+  userperm: 'SEND_MESSAGES',
+  botperm: 'SEND_MESSAGES',
   /**
    * @param {Client} client
    * @param {CommandInteraction} interaction
@@ -12,12 +12,12 @@ module.exports = {
    */
   run: async (client, interaction, args) => {
     const embed = new MessageEmbed()
-      .setTitle("Support the Developers!")
+      .setTitle('Support the Developers!')
       .setDescription(
-        "Support or Donate to the Developers and Get Premium Perks!\n\n[Buy me a Coffee](https://ko-fi.com/gifaldyazkaa)"
+        'Support or Donate the Developers to help keeping this project alive!\n\n[Buy me a Coffee](https://ko-fi.com/gifaldyazkaa)'
       )
       .setFooter(interaction.user.tag)
-      .setColor("BLUE")
+      .setColor('BLUE')
       .setTimestamp();
 
     interaction.followUp({ embeds: [embed] });
