3a36bbd903a99e865222170724b6d414e3c4c3bb
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-27 06:38:47 +0700

    chore(v2): add welcoming category

---
+++ commands/welcoming/check-channel.js
@@ -0,0 +1,28 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const Schema = require('../../models/welcome-channel');
+
+module.exports = {
+  name: 'check-welcome',
+  description: 'Check where is Welcoming System is Enabled',
+  aliases: ['check-welcoming'],
+  emoji: '',
+  userperm: ['MANAGE_GUILD'],
+  botperm: ['SEND_MESSAGES'],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
+      if (!data)
+        return message.reply({ content: `This guild has no data stored.` });
+
+      const channel = client.channels.cache.get(data.Channel);
+
+      message.reply({
+        content: `Welcoming Channel Has Been Set To => ${channel}. \n\nThis bot will automatically sends a Welcoming message to ${channel} when someone joins to this server!`,
+      });
+    });
+  },
+};
