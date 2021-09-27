f9d3e1e3cabc8dea6bb7140391b6483f832d98ac
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-27 14:15:38 +0700

    feat(v2): add search category for normal

---
+++ commands/search/anime-search.js
@@ -0,0 +1,68 @@
+const { Client, Message, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const anilist = require('anilist-node');
+const Anilist = new anilist();
+
+module.exports = {
+  name: 'anime-src',
+  description: 'Search an Anime from Image',
+  aliases: ['anime-img-src', 'anime-image-search'],
+  emoji: '',
+  userperm: ['SEND_MESSAGES'],
+  botperm: ['SEND_MESSAGES'],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    // Get Image from User. Uploaded image or Image URL.
+    const image =
+      message.attachments.size > 0
+        ? message.attachments.map((attachments) => attachments.url)
+        : args.join(' ');
+
+    // If there is no image, Reply and tell the user.
+    if (!image)
+      return message.reply({
+        content: `Please send a Image or Image URL!\nExample : \`${client.config.prefix}search https://gifaldyazka.is-a.dev/image/demo.png\`\nOr upload an image with \`${client.config.prefix}search\` caption.`,
+      });
+
+    // Fetch data using node-fetch.
+    fetch(
+      `https://api.trace.moe/search?cutBorders&url=${encodeURIComponent(image)}`
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        // Try-catch block
+        try {
+          Anilist.media.anime(body.result[0].anilist).then((data) => {
+            const embed = new MessageEmbed()
+              .setAuthor(`Okay, Got it!`, client.user.displayAvatarURL({}))
+              .addField(`Title`, `${data.title.romaji}`)
+              .addField(`Similarity`, `${body.result[0].similarity}`)
+              .addField(`Episode(s)`, `${data.episodes}`)
+              .addField(`Type`, `${data.format}`)
+              .addField(`Status`, `${data.status}`)
+              .setThumbnail(data.coverImage.large)
+              .setImage(body.result[0].image)
+              .setFooter(`Search Request by ${message.author.username}`)
+              .setTimestamp()
+              .setColor('#800080');
+            message.channel.send({ embeds: [embed] });
+          });
+        } catch (err) {
+          // If there is any errors, It will not crashing your process. It'll catch and tell the error.
+          const embed = new MessageEmbed()
+            .setTitle(':x: That Anime is Not Found!')
+            .setDescription(
+              `Something went wrong. Maybe the result is not found.\n\`\`\`yml\n${err}\n\`\`\``
+            )
+            .setColor('#FF0000')
+            .setTimestamp();
+          message.channel.send({ embeds: [embed] });
+          console.log(err);
+        }
+      });
+  },
+};
---
+++ commands/search/maps.js
@@ -0,0 +1,37 @@
+const { Client, Message, MessageAttachment } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = {
+  name: 'maps',
+  description: 'Returns information about a location',
+  aliases: ['gmaps'],
+  emoji: '',
+  userperm: ['SEND_MESSAGES'],
+  botperm: ['SEND_MESSAGES'],
+  /**
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    const sit = args.join(' ');
+    if (!args.length)
+      return message.reply({ content: 'Provide a valid location' });
+    const site = `https://maps.google.com/?q=${args.join('+')}`;
+    try {
+      const msg = await message.channel.send({
+        content: '**Please wait...** This may take up to 10 seconds.',
+      });
+      msg.delete({ timeout: 5000 });
+      const { body } = await fetch(
+        `https://image.thum.io/get/width/1920/crop/675/noanimate/${site}`
+      );
+      let att = new MessageAttachment(body, `${sit}.png`);
+      return message.channel.send({ files: [att] });
+    } catch (err) {
+      return message.reply({
+        content: `Oh no, an error occurred: \`${err.message}\`. Try again later!`,
+      });
+    }
+  },
+};
