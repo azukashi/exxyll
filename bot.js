241fb69fcc6984abfb9befbcc0d41079e281a29d
Gifaldy Azka <falcxxdev@gmail.com>
2021-09-29 14:45:05 +0700

    chore(v1): loads env with dotenv

--- bot.js
+++ bot.js
@@ -1,19 +1,17 @@
-// This file is used for sharding the bot.
-// Just run `node bot.js` to shard the bot.
-// Just do this if the bot is over 2000 servers.
+const { ShardingManager } = require('discord.js');
+const chalk = require('chalk');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { ShardingManager } = require("discord.js");
-const chalk = require("chalk");
-
-const manager = new ShardingManager("./index.js", {
-  totalShards: "auto",
-  shardList: "auto",
-  token: process.env.TOKEN,
+const manager = new ShardingManager('./index.js', {
+  totalShards: 'auto',
+  shardList: 'auto',
+  token: process.env.TOKENDEV,
 });
 
-manager.on("shardCreate", async (shard) => {
+manager.on('shardCreate', async (shard) => {
   console.log(
-    chalk.cyan("[Information] ") +
+    chalk.cyan('[Information] ') +
       chalk.blue(`${new Date()} Spawned ${shard.id}`)
   );
 });
