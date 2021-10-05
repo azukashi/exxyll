b0c7f0a649da3e7d1471a3cce95a8ed3771eba91
Gifaldy Azka <falcxxdev@gmail.com>
2021-10-05 10:59:12 +0700

    chore(v1): cleanup repo

--- handlers/event.js
+++
@@ -1,9 +0,0 @@
-const { readdirSync } = require("fs");
-
-module.exports = (client) => {
-  readdirSync("./events/").forEach((file) => {
-    if (!file.endsWith(".js")) return; // filter the file
-
-    require(`../events/${file}`)(client); // load event
-  });
-};
--- models/welcomeChannel.js
+++
@@ -1,7 +0,0 @@
-const mongo = require("mongoose");
-const Schema = new mongo.Schema({
-    Guild: String,
-    Channel: String,
-});
-
-module.exports = mongo.model('welcome-channel', Schema);
