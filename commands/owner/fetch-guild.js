1183288e03f26c4f39e24e4184a5fa918884e31d
Gifaldy Azka <falcxxdev@gmail.com>
2021-10-11 11:37:06 +0700

    chore(v2): some changes with fetch-guild

--- commands/owner/fetch-guild.js
+++ commands/owner/fetch-guild.js
@@ -14,41 +14,37 @@ module.exports = {
    * @param {String[]} args
    */
   run: async (client, message, args) => {
-    if(!message.author.id === process.env.OWNERID) return;
-    let clientGuilds = message.client.guilds.cache;
-    try {
-      let messageObj;
-      messageObj = Util.splitMessage(
-        clientGuilds.map((g) => {
-          `\`${g.id}\` **|** \`${g.name}\` **|** \`${g.members.cache.size}\``;
-        })
-      );
+    if (!message.author.id === process.env.OWNERID) return;
+    let clientGuilds = client.guilds.cache;
+    let messageObj;
+    messageObj = clientGuilds.map((g) => {
+      `\`${g.id}\` **|** \`${g.name}\` **|** \`${g.members.cache.size}\``;
+    });
 
-      if (messageObj.length == 1) {
+    console.log(messageObj)
+
+    if (messageObj.length == 1) {
+      const embed = new MessageEmbed()
+        .setTitle(`Showing Guild List`)
+        .setDescription(
+          `Guild ID | Guild Name | Total Members\n${messageObj[0]}`
+        )
+        .setColor('#800080');
+      message.channel.send({
+        embeds: [embed],
+      });
+    } else {
+      for (i = 0; messageObj.length < i; i++) {
         const embed = new MessageEmbed()
           .setTitle(`Showing Guild List`)
           .setDescription(
-            `Guild ID | Guild Name | Total Members\n${messageObj[0]}`
+            `Guild ID | Guild Name | Total Members\n${messageObj[i]}`
           )
           .setColor('#800080');
         message.channel.send({
           embeds: [embed],
         });
-      } else {
-        for (i = 0; messageObj.length < i; i++) {
-          const embed = new MessageEmbed()
-            .setTitle(`Showing Guild List`)
-            .setDescription(
-              `Guild ID | Guild Name | Total Members\n${messageObj[i]}`
-            )
-            .setColor('#800080');
-          message.channel.send({
-            embeds: [embed],
-          });
-        }
       }
-    } catch (err) {
-      console.log(err);
     }
   },
 };
