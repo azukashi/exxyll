e5a0cc72bc4d1fc09246cfd2294233bade59e1fc
Gifaldy Azka <falcxxdev@pm.me>
2022-05-14 16:37:47 +0700

    refactor: 💡 run config fnc when importing modules
    Instead of doing const dotenv = require(...); dotenv.config(), run
    config function when requiring module

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { ShardingManager } = require('discord.js');
 const chalk = require('chalk');
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
 
 const manager = new ShardingManager('./index.js', {
   totalShards: 'auto',
