121c754a531036694d632648450cf3f001ae99c2
Gifaldy Azka <me@falcxxdev.ml>
2022-05-15 15:41:50 +0700

    feat: 🎸 use moment to format date

--- bot.js
+++ bot.js
@@ -1,18 +1,21 @@
 const { ShardingManager } = require('discord.js');
+const moment = require('moment');
 const chalk = require('chalk');
 require('dotenv').config();
 
 const manager = new ShardingManager('./index.js', {
-  totalShards: 'auto',
-  shardList: 'auto',
-  token: process.env.TOKEN,
+	totalShards: 'auto',
+	shardList: 'auto',
+	token: process.env.TOKEN,
 });
 
 manager.on('shardCreate', async (shard) => {
-  console.log(
-    chalk.cyan('[Information] ') +
-      chalk.blue(`${new Date()} Spawned ${shard.id}`)
-  );
+	console.log(
+		chalk.grey('[INFO] ') +
+			chalk.blueBright(
+				`${moment(new Date()).format('llll')} Spawned ${shard.id}`
+			)
+	);
 });
 
 manager.spawn();
