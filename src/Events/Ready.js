200519070d7075cd549b6c38d667a912c09c275a
Azka <falcxxdev@pm.me>
2025-02-09 03:26:44 +0000

    chore(ready): set status to idle

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -4,13 +4,14 @@ const chalk = require('chalk');
 const prefix = process.env.PREFIX;
 
 client.on('ready', async () => {
+    client.user.setStatus('idle');
     client.user.setActivity(`${prefix}help | Live at ${client.guilds.cache.size} servers`, {
         type: 'LISTENING',
     });
 
     console.log(
         `${chalk.grey.bold('[INFO]  ')}${chalk.blueBright.bold(client.user.tag)} ${chalk.white(
-            'is',
-        )} ${chalk.green.bold('Online')}`,
+            'is'
+        )} ${chalk.green.bold('Online')}`
     );
 });
